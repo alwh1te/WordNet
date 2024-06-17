13ffe29a70915a5757cd88b2094d0c3d4c9ca65b
alw <alexander.s.filatov@gmail.com>
2024-06-17 17:28:34 +0300

    init

---
+++ libraries/wordnet/include/wordnet/Wordnet.hpp
@@ -0,0 +1,104 @@
+#ifndef WORDNET_WORDNET_HPP
+#define WORDNET_WORDNET_HPP
+
+#include <iosfwd>
+#include <set>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <limits>
+#include <queue>
+#include <iterator>
+
+class Digraph {
+private:
+    int V;
+    int E;
+    std::vector<std::list<int>> adj;
+
+public:
+    Digraph(int V);
+    int vertex_count() const;
+    int edge_count() const;
+    void add_edge(int v, int w);
+    const std::list<int>& adjacent(int v) const;
+    friend std::ostream& operator<<(std::ostream& os, const Digraph& graph);
+};
+
+class ShortestCommonAncestor {
+private:
+    const Digraph& G;
+
+    struct BFSResult {
+        std::vector<int> dist;
+        std::vector<int> edgeTo;
+        BFSResult(int V);
+    };
+
+    BFSResult bfs(int s) const;
+
+public:
+    explicit ShortestCommonAncestor(const Digraph& dg);
+    unsigned length(unsigned v, unsigned w);
+    unsigned ancestor(unsigned v, unsigned w);
+    unsigned length_subset(const std::set<unsigned>& subset_a, const std::set<unsigned>& subset_b);
+    unsigned ancestor_subset(const std::set<unsigned>& subset_a, const std::set<unsigned>& subset_b);
+};
+
+class WordNet {
+private:
+    std::unordered_map<std::string, std::vector<int>> noun_to_synsets;
+    std::unordered_map<int, std::vector<std::string>> synset_to_nouns;
+    std::unordered_map<int, std::string> synset_to_gloss;
+    Digraph G;
+
+public:
+    WordNet(std::istream& synsets, std::istream& hypernyms);
+
+    class Nouns {
+    private:
+        std::unordered_set<std::string> nouns;
+
+    public:
+        class iterator {
+        private:
+            std::unordered_set<std::string>::const_iterator it;
+        public:
+            using iterator_category = std::forward_iterator_tag;
+            using value_type = std::string;
+            using difference_type = std::ptrdiff_t;
+            using pointer = const std::string*;
+            using reference = const std::string&;
+
+            iterator() = default;
+            iterator(std::unordered_set<std::string>::const_iterator it);
+            iterator& operator++();
+            iterator operator++(int);
+            bool operator==(const iterator& other) const;
+            bool operator!=(const iterator& other) const;
+            reference operator*() const;
+            pointer operator->() const;
+        };
+
+        Nouns(const std::unordered_map<std::string, std::vector<int>>& noun_map);
+        iterator begin() const;
+        iterator end() const;
+    };
+
+    Nouns nouns() const;
+    bool is_noun(const std::string& word) const;
+    std::string sca(const std::string& noun1, const std::string& noun2) const;
+    unsigned distance(const std::string& noun1, const std::string& noun2) const;
+};
+
+class Outcast {
+private:
+    WordNet& wordnet;
+public:
+    explicit Outcast(WordNet& wordnet);
+    std::string outcast(const std::set<std::string>& nouns);
+};
+
+#endif  // WORDNET_WORDNET_HPP
---
+++ libraries/wordnet/src/Wordnet.cpp
@@ -0,0 +1,278 @@
+#include "/Users/alw/CLionProjects/WordNet/libraries/wordnet/include/wordnet/Wordnet.hpp"
+#include <sstream>
+
+// Digraph Implementation
+
+Digraph::Digraph(int V) : V(V), E(0), adj(V) {}
+
+int Digraph::vertex_count() const {
+    return V;
+}
+
+int Digraph::edge_count() const {
+    return E;
+}
+
+void Digraph::add_edge(int v, int w) {
+    adj[v].push_back(w);
+    E++;
+}
+
+const std::list<int>& Digraph::adjacent(int v) const {
+    return adj[v];
+}
+
+std::ostream& operator<<(std::ostream& os, const Digraph& graph) {
+    for (int v = 0; v < graph.V; v++) {
+        os << v << ": ";
+        for (int w : graph.adj[v]) {
+            os << w << " ";
+        }
+        os << std::endl;
+    }
+    return os;
+}
+
+
+ShortestCommonAncestor::BFSResult::BFSResult(int V) : dist(V, std::numeric_limits<int>::max()), edgeTo(V, -1) {}
+
+ShortestCommonAncestor::ShortestCommonAncestor(const Digraph& dg) : G(dg) {}
+
+ShortestCommonAncestor::BFSResult ShortestCommonAncestor::bfs(int s) const {
+    BFSResult result(G.vertex_count());
+    std::queue<int> q;
+    result.dist[s] = 0;
+    q.push(s);
+
+    while (!q.empty()) {
+        int v = q.front();
+        q.pop();
+
+        for (int w : G.adjacent(v)) {
+            if (result.dist[w] == std::numeric_limits<int>::max()) {
+                result.dist[w] = result.dist[v] + 1;
+                result.edgeTo[w] = v;
+                q.push(w);
+            }
+        }
+    }
+
+    return result;
+}
+
+unsigned ShortestCommonAncestor::length(unsigned v, unsigned w) {
+    return length_subset({v}, {w});
+}
+
+unsigned ShortestCommonAncestor::ancestor(unsigned v, unsigned w) {
+    return ancestor_subset({v}, {w});
+}
+
+unsigned ShortestCommonAncestor::length_subset(const std::set<unsigned>& subset_a, const std::set<unsigned>& subset_b) {
+    std::vector<int> dist_to_a(G.vertex_count(), std::numeric_limits<int>::max());
+    std::vector<int> dist_to_b(G.vertex_count(), std::numeric_limits<int>::max());
+
+    for (unsigned v : subset_a) {
+        BFSResult bfs_result = bfs(v);
+        for (int i = 0; i < G.vertex_count(); ++i) {
+            dist_to_a[i] = std::min(dist_to_a[i], bfs_result.dist[i]);
+        }
+    }
+
+    for (unsigned v : subset_b) {
+        BFSResult bfs_result = bfs(v);
+        for (int i = 0; i < G.vertex_count(); ++i) {
+            dist_to_b[i] = std::min(dist_to_b[i], bfs_result.dist[i]);
+        }
+    }
+
+    int min_length = std::numeric_limits<int>::max();
+    for (int i = 0; i < G.vertex_count(); ++i) {
+        if (dist_to_a[i] != std::numeric_limits<int>::max() && dist_to_b[i] != std::numeric_limits<int>::max()) {
+            min_length = std::min(min_length, dist_to_a[i] + dist_to_b[i]);
+        }
+    }
+
+    return min_length;
+}
+
+unsigned ShortestCommonAncestor::ancestor_subset(const std::set<unsigned>& subset_a, const std::set<unsigned>& subset_b) {
+    std::vector<int> dist_to_a(G.vertex_count(), std::numeric_limits<int>::max());
+    std::vector<int> dist_to_b(G.vertex_count(), std::numeric_limits<int>::max());
+
+    for (unsigned v : subset_a) {
+        BFSResult bfs_result = bfs(v);
+        for (int i = 0; i < G.vertex_count(); ++i) {
+            dist_to_a[i] = std::min(dist_to_a[i], bfs_result.dist[i]);
+        }
+    }
+
+    for (unsigned v : subset_b) {
+        BFSResult bfs_result = bfs(v);
+        for (int i = 0; i < G.vertex_count(); ++i) {
+            dist_to_b[i] = std::min(dist_to_b[i], bfs_result.dist[i]);
+        }
+    }
+
+    int min_length = std::numeric_limits<int>::max();
+    int ancestor = -1;
+    for (int i = 0; i < G.vertex_count(); ++i) {
+        if (dist_to_a[i] != std::numeric_limits<int>::max() && dist_to_b[i] != std::numeric_limits<int>::max()) {
+            int length = dist_to_a[i] + dist_to_b[i];
+            if (length < min_length) {
+                min_length = length;
+                ancestor = i;
+            }
+        }
+    }
+
+    return ancestor;
+}
+
+// WordNet Implementation
+
+WordNet::WordNet(std::istream& synsets, std::istream& hypernyms) : G(0) {
+    std::string line;
+    int max_synset_id = 0;
+
+    // Reading synsets
+    while (std::getline(synsets, line)) {
+        std::stringstream ss(line);
+        std::string id_str, synonyms, gloss;
+        std::getline(ss, id_str, ',');
+        std::getline(ss, synonyms, ',');
+        std::getline(ss, gloss);
+
+        int id = std::stoi(id_str);
+        max_synset_id = std::max(max_synset_id, id);
+
+        synset_to_gloss[id] = gloss;
+
+        std::stringstream ss_synonyms(synonyms);
+        std::string noun;
+        while (ss_synonyms >> noun) {
+            noun_to_synsets[noun].push_back(id);
+            synset_to_nouns[id].push_back(noun);
+        }
+    }
+
+    // Creating graph with the necessary number of vertices
+    G = Digraph(max_synset_id + 1);
+
+    // Reading hypernyms
+    while (std::getline(hypernyms, line)) {
+        std::stringstream ss(line);
+        std::string id_str;
+        std::getline(ss, id_str, ',');
+
+        int id = std::stoi(id_str);
+        std::string hypernym_id_str;
+        while (std::getline(ss, hypernym_id_str, ',')) {
+            int hypernym_id = std::stoi(hypernym_id_str);
+            G.add_edge(id, hypernym_id);
+        }
+    }
+}
+
+WordNet::Nouns::Nouns(const std::unordered_map<std::string, std::vector<int>>& noun_map) {
+    for (const auto& pair : noun_map) {
+        nouns.insert(pair.first);
+    }
+}
+
+WordNet::Nouns::iterator::iterator(std::unordered_set<std::string>::const_iterator it) : it(it) {}
+
+WordNet::Nouns::iterator& WordNet::Nouns::iterator::operator++() {
+    ++it;
+    return *this;
+}
+
+WordNet::Nouns::iterator WordNet::Nouns::iterator::operator++(int) {
+    iterator temp = *this;
+    ++it;
+    return temp;
+}
+
+bool WordNet::Nouns::iterator::operator==(const iterator& other) const {
+    return it == other.it;
+}
+
+bool WordNet::Nouns::iterator::operator!=(const iterator& other) const {
+    return it != other.it;
+}
+
+WordNet::Nouns::iterator::reference WordNet::Nouns::iterator::operator*() const {
+    return *it;
+}
+
+WordNet::Nouns::iterator::pointer WordNet::Nouns::iterator::operator->() const {
+    return &(*it);
+}
+
+WordNet::Nouns::iterator WordNet::Nouns::begin() const {
+    return iterator(nouns.cbegin());
+}
+
+WordNet::Nouns::iterator WordNet::Nouns::end() const {
+    return iterator(nouns.cend());
+}
+
+WordNet::Nouns WordNet::nouns() const {
+    return Nouns(noun_to_synsets);
+}
+
+bool WordNet::is_noun(const std::string& word) const {
+    return noun_to_synsets.find(word) != noun_to_synsets.end();
+}
+
+std::string WordNet::sca(const std::string& noun1, const std::string& noun2) const {
+    if (!is_noun(noun1) || !is_noun(noun2)) {
+        throw std::invalid_argument("Noun not found in WordNet");
+    }
+
+    std::set<unsigned> synset_ids1(noun_to_synsets.at(noun1).begin(), noun_to_synsets.at(noun1).end());
+    std::set<unsigned> synset_ids2(noun_to_synsets.at(noun2).begin(), noun_to_synsets.at(noun2).end());
+
+    ShortestCommonAncestor sca(G);
+    unsigned ancestor_id = sca.ancestor_subset(synset_ids1, synset_ids2);
+
+    return synset_to_gloss.at(ancestor_id);
+}
+
+unsigned WordNet::distance(const std::string& noun1, const std::string& noun2) const {
+    if (!is_noun(noun1) || !is_noun(noun2)) {
+        throw std::invalid_argument("Noun not found in WordNet");
+    }
+
+    std::set<unsigned> synset_ids1(noun_to_synsets.at(noun1).begin(), noun_to_synsets.at(noun1).end());
+    std::set<unsigned> synset_ids2(noun_to_synsets.at(noun2).begin(), noun_to_synsets.at(noun2).end());
+
+    ShortestCommonAncestor sca(G);
+    return sca.length_subset(synset_ids1, synset_ids2);
+}
+
+// Outcast Implementation
+
+Outcast::Outcast(WordNet& wordnet) : wordnet(wordnet) {}
+
+std::string Outcast::outcast(const std::set<std::string>& nouns) {
+    int max_distance = -1;
+    std::string outcast_word;
+
+    for (const std::string& noun : nouns) {
+        int dist_sum = 0;
+
+        for (const std::string& other_noun : nouns) {
+            if (noun != other_noun) {
+                dist_sum += wordnet.distance(noun, other_noun);
+            }
+        }
+
+        if (dist_sum > max_distance) {
+            max_distance = dist_sum;
+            outcast_word = noun;
+        }
+    }
+
+    return outcast_word;
+}
